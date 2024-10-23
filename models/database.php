13532d7e4e86960d9818c2fea2c948726f09a93c
Diogo Duarte <diogod.b.lago01@gmail.com>
2024-10-23 20:17:29 -0300

    Update database.php

--- models/database.php
+++ models/database.php
@@ -3,7 +3,7 @@ class Database {
     private static $host = 'localhost';
     private static $dbName = 'amigo_secreto';
     private static $username = 'root';
-    private static $password = '';
+    private static $password = 'root';
     private static $conn = null;
 
     public static function connect() {
